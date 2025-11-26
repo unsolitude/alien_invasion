86cd45ce6ae1f044d2dd47ef79e3f6773ac7c04c
unsolitude <kangzj2025@shanghaitech.edu.cn>
2025-11-26 23:44:59 +0800

    finish chapter 13

--- src/game_stats.py
+++ src/game_stats.py
@@ -1,7 +1,7 @@
 class GameStats:
     """跟踪游戏的统计信息"""
 
-    def __init__(self):
+    def __init__(self,ai_game):
         """初始化统计信息"""
         self.settings = ai_game.settings
         self.reset_stats()
