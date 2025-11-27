555dc7d7fcb4ec01d4fd9a90a154049ffa444145
unsolitude <kangzj2025@shanghaitech.edu.cn>
2025-11-27 14:32:26 +0800

    finish 14

--- src/settings.py
+++ src/settings.py
@@ -8,18 +8,15 @@ class Settings:
         self.bg_color = (230,230,230)
 
         #飞船设置
-        self.ship_speed = 1.5
         self.ship_limit = 3
 
         #子弹设置
-        self.bullet_speed = 2.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60,60,60)
         self.bullet_allowed = 10
 
         #外星人设置
-        self.alien_speed = 1.0
         self.fleet_drop_speed = 30
         self.fleet_direction = 1
 
@@ -30,8 +27,8 @@ class Settings:
 
     def initialize_dynamic_settings(self):
         """初始化随游戏进行而变化的设置"""
-        self.ship_speed = 1.5
-        self.bullet_speed = 2.5
+        self.ship_speed = 5
+        self.bullet_speed = 4
         self.alien_speed = 1.0
         self.alien_points = 50
 
